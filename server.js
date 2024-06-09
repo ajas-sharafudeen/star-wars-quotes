72c60b03638b1b128794e2ecabbd1ed4646a893d
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-06-09 18:38:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 3000
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://ajassharaf:cybQA8RGfFTZZ@cluster0.ll1kd6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true";
+
+MongoClient.connect(uri, ({ useUnifiedTopology: true }))
+  .then(client => {
+    console.log('Connected to Database!!!')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
+
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.get('/', (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.log(error))
+      res.render('index.ejs', {})
+    })
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.log(error))
+    })
+    app.listen(PORT, () => {
+      console.log(`Listening on PORT ${PORT}`);
+    })
+  })
+  .catch(error => console.error(error))
